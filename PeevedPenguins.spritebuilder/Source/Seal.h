88705cb3b895990d8e92209a19f780f17b1c0bdb
John Preston Chua Cua <bliss@Johns-MacBook-Pro.local>
2014-10-23 00:07:16 +0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by John Preston Chua Cua on 23/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
